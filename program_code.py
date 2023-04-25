41ab03457dc37929e8e92549d0c65ca153da1b2f
Ms Lawliet <alexieanoya@gmail.com>
2023-04-25 13:00:01 +0800

    wrote instructions

---
+++ program_code.py
@@ -0,0 +1,8 @@
+# Write a method in python to write multiple line of text contents into a text file mylife.txt.
+# Sample output:
+# Enter line: Beautiful is better than ugly.
+# Are there more lines y/n? y
+# Enter line: Explicit is better than implicit.
+# Are there more lines y/n? y
+# Enter line: Simple is better than complex.
+# Are there more lines y/n? n
