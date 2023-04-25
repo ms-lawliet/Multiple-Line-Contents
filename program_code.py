9dc771d33b28ead9770984c029c971fbdf5d8546
Ms Lawliet <alexieanoya@gmail.com>
2023-04-25 13:02:19 +0800

    wrote pseudocode

--- program_code.py
+++ program_code.py
@@ -6,3 +6,12 @@
 # Are there more lines y/n? y
 # Enter line: Simple is better than complex.
 # Are there more lines y/n? n
+
+# open text file
+# ask user for input
+# wwrite input into mylife.txt
+# ask if there is more
+# if no (n),
+# stop execution
+# if yes (y),
+# ask user for another input
